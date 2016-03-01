0b1e1d9cccd31a1a3dba11f901ecea1143fc6591
Cristian Georgescu <nocode66@yahoo.com>
2016-03-01 13:46:46 +0000

    initial api setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+constraints subdomain: 'api' do
+  scope module: 'api' do
+    namespace :v1 do
 
-  # Serve websocket cable requests in-process
-  # mount ActionCable.server => '/cable'
+      #add resources here
+
+    end
+  end
+end
 end
